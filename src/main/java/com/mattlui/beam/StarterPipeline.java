167d313a0c0aa5f2aa1236f7e224717ed2accf43
Matt Lui <matthew.lui@anz.com>
2021-09-17 16:03:39 +1000

    Add Maven Beam starter archetype
    Builds successfully with jdk11/java8

--- src/main/java/com/mattlui/beam/StarterPipeline.java
+++ src/main/java/com/mattlui/beam/StarterPipeline.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.examples;
+package com.mattlui.beam;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
